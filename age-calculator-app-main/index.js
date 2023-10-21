94b4de8626e0d030a6c4f537b7ef483c282341bd
oyeyemiife <ifeoluwaoyeyemi915@gmail.com>
2023-10-21 12:51:33 +0100

    updated-calculator

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -1,90 +1,125 @@
-//output
-const years = document.querySelector(".years");
-const months = document.querySelector(".months");
-const days = document.querySelector(".days");
-const btn = document.querySelector(".btn")
-
-//input
-let numbers = false
-const YYYY = document.querySelector("#year");
-const MM = document.querySelector("#month");
-const DD = document.querySelector("#day");
-
-const ey = document.querySelector(".ey");
-const em = document.querySelector(".em");
-const ed = document.querySelector(".ed");
-
-btn.addEventListener('click', calculate)
-DD.addEventListener("input", (e) => {
-    if (+DD.value > 31) {
-        ed.textContent = "Must be a valid day";
-        numbers = false;
-        return;
-    } else {
-        numbers = true;
-        ed.textContent = "";
+function isValidDay(day) {
+    if (day > 31) {
+      return "Must be a valid day";
     }
-    if (+DD.value === 0) {
-        numbers = false;
-        ed.textContent = "This field is required";
-        numbers = false;
-        return;
-    } else {
-        ed.textContent = "";
+    if (day === 0 || !day) {
+      return "This field is required";
     }
-});
-MM.addEventListener("input", (e) => {
-    if (+MM.value > 12) {
-        em.textContent = "Must be a valid month";
-        numbers = false;
-        return;
-    } else {
-        numbers = true;
-        em.textContent = "";
+    return false;
+  }
+  
+  function isValidMonth(month) {
+    if (month > 12) {
+      return "Must be a valid month";
     }
-    if (+MM.value === 0) {
-        numbers = false;
-        em.textContent = "This field is required";
-        numbers = false;
-        return;
-    } else {
-        em.textContent = "";
+    if (month === 0 || !month) {
+      return "This field is required";
     }
-});
-YYYY.addEventListener("input", (e) => {
-    if (+YYYY.value > 2023) {
-        ey.textContent = "Must be a valid year";
-        numbers = false;
-        return;
-    } else {
-        numbers = true;
-        ed.textContent = "";
+    return false;
+  }
+  
+  function isValidYear(year) {
+    const currentYear = new Date().getFullYear();
+    if (year > currentYear) {
+      return "Must be a valid year";
     }
-    if (+YYYY.value === 0) {
-        numbers = false;
-        ey.textContent = "This field is required";
-        numbers = false;
-        return;
-    } else {
-        ey.textContent = "";
+    if (year === 0 || !year) {
+      return "This field is required";
     }
-});
-
-function calculate() {
-    if (numbers) {
-        let birth = '${MM.value}/${DD.value}/${YYYY.value}';
-        console.log(birth);
-        let birthObj = new date(birth);
-        let ageDiffMill = date.now() - birthObj;
-        let agedate = new date(ageDiffMill);
-        let ageyears = agedate.getUTCFullyear() - 1970;
-        let agemonth = agedate.getUTCmonth();
-        let ageday = agedate.getUTCday() - 1;
-        days.textContent = ageday;
-        months.textContent = agemonth;
-        years.textContent = ageyears;
-    } 
-    else {
-        alert("error");
+    return false;
+  }
+  
+  
+  const birthYearInput = document.getElementById("birth_year");
+  const birthMonthInput = document.getElementById("birth_month");
+  const birthDayInput = document.getElementById("birth_day");
+  
+  const birthYearError = document.getElementById("birth_year_error");
+  const birthMonthError = document.getElementById("birth_month_error");
+  const birthDayError = document.getElementById("birth_day_error");
+  
+  birthDayInput.addEventListener("input", () => {
+    const errorMessage = isValidDay(birthDayInput.value);
+    birthDayError.textContent = errorMessage || "";
+  });
+  
+  birthMonthInput.addEventListener("input", () => {
+    const errorMessage = isValidMonth(birthMonthInput.value);
+    birthMonthError.textContent = errorMessage || "";
+  });
+  
+  birthYearInput.addEventListener("input", () => {
+    const errorMessage = isValidYear(birthYearInput.value);
+    birthYearError.textContent = errorMessage || "";
+  });
+  
+  const calculateAge = (day, month, year) => {
+    const birthday = `${day}/${month}/${year}`;
+  
+    const birthdate = new Date(year, month - 1, day); 
+  
+    const currentDate = new Date();
+  
+    let years = currentDate.getFullYear() - birthdate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const birthMonth = birthdate.getMonth();
+  
+    if (
+      currentMonth < birthMonth ||
+      (currentMonth === birthMonth && currentDate.getDate() < birthdate.getDate())
+    ) {
+      years--;
     }
-}
+  
+    const months = (currentDate.getMonth() - birthdate.getMonth() + 12) % 12;
+  
+    const days = Math.abs(currentDate.getDate() - birthdate.getDate());
+  
+    return {
+      years: years,
+      months: months,
+      days: days,
+    };
+  };
+  
+  function validateForm() {
+    const isYearValid = isValidYear(birthYearInput.value);
+    const isMonthValid = isValidMonth(birthMonthInput.value);
+    const isDayValid = isValidDay(birthDayInput.value);
+  
+    birthYearError.textContent = isYearValid || "";
+    birthMonthError.textContent = isMonthValid || "";
+    birthDayError.textContent = isDayValid || "";
+  
+    if (isYearValid || isMonthValid || isDayValid) {
+      alert("Please fill in all fields correctly.");
+      return false;
+    }
+  
+    return true;
+  }
+  
+  const ageForm = document.getElementById("ageForm");
+  
+  const ageYearResult = document.getElementById("age_year");
+  const ageMonthResult = document.getElementById("age_month");
+  const ageDayResult = document.getElementById("age_day");
+  
+  ageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const isValid = validateForm();
+  
+    if (isValid) {
+      const age = calculateAge(
+        birthDayInput.value,
+        birthMonthInput.value,
+        birthYearInput.value
+      );
+  
+      ageYearResult.textContent = age.years;
+      ageMonthResult.textContent = age.months;
+      ageDayResult.textContent = age.days;
+    }
+  });
+  
